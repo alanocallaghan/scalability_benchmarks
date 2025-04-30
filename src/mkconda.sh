d108a81a07f84c555f11939b70cf251fe85d10da
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2025-04-30 11:06:23 +0100

    Add env files

--- src/mkconda.sh
+++ src/mkconda.sh
@@ -1,49 +1,8 @@
-# grep -rh "library(" | sed -e 's/^[ \t]*//' | sort | uniq
-# library("argparse")
-# library("coda")
-# library("dplyr")
-# library("ggbeeswarm")
-# library("ggplot2")
-# library("here")
-# library("viridis")
-# library("BASiCStan")
-# library("BASiCS")
-# library("BiocParallel")
-# library("scater")
-# library("scran")
-# library("SingleCellExperiment")
-
-
-conda create -n scalability \
-    gcc \
-    r-base=4.1.1 \
-    r-argparse \
-    r-curl \
-    r-httr \
-    r-coda \
-    r-dplyr \
-    r-ggbeeswarm \
-    r-ggplot2 \
-    r-here \
-    r-viridis \
-    snakemake \
-    r-biocmanager \
-    r-devtools \
-    r-rstan \
-    r-ggpointdensity \
-    r-rcpparmadillo \
-    r-ggrastr \
-    r-patchwork \
-    bioconductor-scrnaseq
-    # bioconductor-basics \
-    # bioconductor-scater \
-    # bioconductor-scran \
-    # bioconductor-singlecellexperiment \
-
+conda env create -n scalability -f scalability.yml
 
 ## also need to devtools::install_github some stuff
+conda activate scalability
 
-# mamba activate scalability
 Rscript -e 'BiocManager::install(c("BASiCS", "BiocParallel", "scater", "scran", "SingleCellExperiment", "scRNAseq"), version=3.14)'
 
 Rscript -e 'devtools::install_github("catavallejos/BASiCS", ref="49186bda2c5e410a87c9dd8e19d20310c97e5465")'
@@ -51,3 +10,5 @@ Rscript -e 'devtools::install_github("Alanocallaghan/BASiCStan", ref="9e632610cf
 Rscript -e 'devtools::install_github("jorainer/ensembldb", ref="058be8a")'
 Rscript -e 'devtools::install_github("Bioconductor/BiocFileCache", ref="004cb8e")'
 Rscript -e 'install.packages("patchwork")'
+
+conda env create -n datasets -f data-env.yml
